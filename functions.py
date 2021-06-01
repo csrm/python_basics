9d37667eac1c85b3f291fedd6c8dec83a68caf1e
Chivukula Sree Mrudula <chivukula.mrudula@gmail.com>
2021-06-01 17:12:55 +0530

    Python continuation...

---
+++ functions.py
@@ -0,0 +1,5 @@
+def greeting_user():
+	#Print statement to wish a user
+	print("Hello, User!!")
+
+greeting_user()
---
+++ remove_list.py
@@ -0,0 +1,8 @@
+#List of Fruits
+fruits = ['Apple', 'Banana', 'Orange', 'Grapes']
+#print the list
+print('Original List:',fruits)
+#Remove an Element in the list based on a value
+fruits.remove('Banana')
+#print the list
+print('List after removal of an element by value:',fruits)
